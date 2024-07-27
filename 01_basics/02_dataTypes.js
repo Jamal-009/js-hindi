fe89fe0d27f0d79be90c5f1f87555534a741f4fc
Jamal-009 <jamaljamali11220@gmail.com>
2024-07-27 11:53:20 +0500

    Add files via upload

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // not like this..! code readabilty should be high
+
+console.log("Hitesh")
+
+let name = "Jamal"
+let age = 18
+let isLoggedIn = false
+let state = 23
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
